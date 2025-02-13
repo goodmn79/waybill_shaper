6e9b6c4a6c1eb5e5ea19a7237beed74a7670a997
goodmn79 <goodmn79@gmail.com>
2025-02-13 14:53:30 +0300

    Initial commit

---
+++ src/test/java/com/goodmn/waybill_shaper/WaybillShaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodmn.waybill_shaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaybillShaperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
