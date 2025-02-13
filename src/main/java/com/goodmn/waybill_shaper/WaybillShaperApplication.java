da8e36a72033a4e85e1a4b418668abe449631466
goodmn79 <goodmn79@gmail.com>
2025-02-13 14:46:05 +0300

    Initial commit

---
+++ src/main/java/com/goodmn/waybill_shaper/WaybillShaperApplication.java
@@ -0,0 +1,13 @@
+package com.goodmn.waybill_shaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaybillShaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaybillShaperApplication.class, args);
+    }
+
+}
